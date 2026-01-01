62b7207790b432ec4aaf87276fbfe5fd7de80402
Borgesarturr <artur.efivest.10@efivest.com>
2026-01-01 20:31:06 -0300

    Corrige arquivos ignorados pelo git

---
+++ Testes JS/script.js
@@ -0,0 +1,17 @@
+let familia = ["Arnoldo", "Helena", "Erlania", "Netchule", "Ramon", "Artur", "Kezia"]
+
+function verificar() {
+    
+    console.log(familia.length)
+
+}
+
+
+verificar()
+
+let nome = "Artur"
+function saudacao(nome) {
+    console.log(`Olá ${nome}!`)
+}
+
+saudacao()
